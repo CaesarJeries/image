3bab82087c1a0d6708d47db467c9a93a1b7882d4
CaesarJeries <caesar.dakwar@outlook.co.il>
2021-04-03 19:49:28 +0300

    added a simple draw function that uses the SDL2 library / updated cmake config file

--- draw.cpp
+++ draw.cpp
@@ -1,25 +1,67 @@
-#include <GL/glut.h>
+#include <SDL2/SDL.h>
 
-void displayMe(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.5, 0.0, 0.5);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-        glVertex3f(0.0, 0.0, 0.5);
-    glEnd();
-    glFlush();
+#include "image.hpp"
+
+using namespace caesar;
+
+void draw_image(const Image& img) {
+    bool quit = false;
+    SDL_Event event;
+ 
+    SDL_Init(SDL_INIT_VIDEO);
+ 
+    SDL_Window * window = SDL_CreateWindow("SDL2 Pixel Drawing",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+    
+
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Texture * texture = SDL_CreateTexture(renderer,
+                                              SDL_PIXELFORMAT_ARGB8888,
+                                              SDL_TEXTUREACCESS_STATIC,
+                                              img.get_height(), img.get_width());
+
+
+    Uint32 * pixels = new Uint32[img.get_height() * img.get_width()];
+    int pixels_itr = 0;
+    for (int i = 0; i < img.get_height(); ++i) {
+        for (int j = 0; j < img.get_width(); ++j) {
+            pixels[pixels_itr] = img.get(i, j);
+        }
+    }
+
+    SDL_UpdateTexture(texture, NULL, pixels, img.get_height() * sizeof(Uint32));
+
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+ 
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
+    }
+ 
+    delete[] pixels; pixels = nullptr;
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(400, 300);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Hello world!");
-    glutDisplayFunc(displayMe);
-    glutMainLoop();
+
+int main() {
+    Image img(1920, 1080);
+
+    for (int i = 0; i < img.get_height(); ++i) {
+        for (int j = 0; j < img.get_width(); ++j) {
+            img.set(i, j, (byte_t)(i*j) & 0xFF);
+        }
+    }
+
+    draw_image(img);
+
     return 0;
 }
